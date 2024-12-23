b05dea4cffd9fa67dcb4effd33cae8fa66a72dc8
ysam020 <sameery.020@gmail.com>
2024-12-23 09:08:37 +0530

    initial commit

--- app.mjs
+++ app.mjs
@@ -1,11 +1,12 @@
 import connectDB from "./connectDb.mjs";
 import configureApp from "./appConfig.mjs";
-
+import UserModel from "./userModel.mjs";
 const app = configureApp();
 
 connectDB().then((mongooseConnection) => {
-  app.get("/", (req, res) => {
-    res.send("Hello World!");
+  app.get("/", async (req, res) => {
+    const users = await UserModel.find({});
+    res.send(users);
   });
 
   app.listen(9000, () => {
